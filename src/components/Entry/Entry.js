acbc542d8b8693cf9aa527b428ce1f1780166cc1
Sterling Watts <sterling.watts@mindera.com>
2021-01-09 20:41:15 -0800

    Style main pokedex

--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -1,16 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import pokeball from "./pokeball.png"
+import "./Entry.css";
 
 const Entry = (props) => {
-  const {pokedex} = props; 
-  
-  return(
-    <ul>
-    {pokedex.map((pokemon, idx) => {
-      return <li key={idx}>{pokemon.name}</li>
-    })}
-    </ul>
-  )
-}
+  const { pokedex } = props;
+
+  return (
+    <div className="Entry">
+      <div className="container">
+        <ul>
+          {pokedex.map((pokemon, idx) => {
+            let stringIndex = idx.toString()
+            if (stringIndex.length < 2) {
+              stringIndex = "0" + stringIndex
+            } 
+            if (stringIndex.length < 3) {
+              stringIndex = "0" + stringIndex
+            }
+            return <li key={idx}><div>N.° {stringIndex}</div><div>{pokemon.name}</div><div><img src={pokeball} alt="pokeball" height="16px" /></div></li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Entry;
